83aab6795a98364bfec07214667d2d413145b52a
HotDrify <105966726+HotDrify@users.noreply.github.com>
2024-12-14 18:23:27 +0300

    update hikka_info.py

--- injections/hikka_info.py
+++ injections/hikka_info.py
@@ -49,11 +49,9 @@ class HikkaInfoMod(loader.Module):
     def _render_info(self, inline: bool) -> str:
         try:
             repo = git.Repo(search_parent_directories=True)
-            diff = repo.git.log(
-                [f"HEAD..origin/{version.branch}", "--oneline"])
+            diff = repo.git.log([f"HEAD..origin/{version.branch}", "--oneline"])
             upd = (
-                self.strings("update_required") if diff else self.strings(
-                    "up-to-date")
+                self.strings("update_required") if diff else self.strings("up-to-date")
             )
         except Exception:
             upd = ""
@@ -83,52 +81,53 @@ class HikkaInfoMod(loader.Module):
         ]:
             platform = platform.replace(emoji, icon)
 
-        return self.config["custom_message"].format(
-            me=me,
-            version=_version,
-            build=build,
-            prefix=prefix,
-            platform=platform,
-            upd=upd,
-            uptime=utils.formatted_uptime(),
-            cpu_usage=utils.get_cpu_usage(),
-            ram_usage=f"{utils.get_ram_usage()} MB",
-            branch=version.branch,
-        )
-        if self.config["custom_message"]
-        else (
-            f'<b>{{}}</b>\n\n<b>{{}} {self.strings("owner")}:</b> {me}\n\n<b>{{}}'
-            f' {self.strings("version")}:</b> {_version} {build}\n<b>{{}}'
-            f' {self.strings("branch")}:'
-            f"</b> <code>{version.branch}</code>\n{upd}\n\n<b>{{}}"
-            f' {self.strings("prefix")}:</b> {prefix}\n<b>{{}}'
-            f' {self.strings("uptime")}:'
-            f"</b> {utils.formatted_uptime()}\n\n<b>{{}}"
-            f' {self.strings("cpu_usage")}:'
-            f"</b> <i>~{utils.get_cpu_usage()} %</i>\n<b>{{}}"
-            f' {self.strings("ram_usage")}:'
-            f"</b> <i>~{utils.get_ram_usage()} MB</i>\n<b>{{}}</b>"
-        ).format(
-            *map(
-                lambda x: utils.remove_html(x) if inline else x,
-                (
+        return (
+            self.config["custom_message"].format(
+                me=me,
+                version=_version,
+                build=build,
+                prefix=prefix,
+                platform=platform,
+                upd=upd,
+                uptime=utils.formatted_uptime(),
+                cpu_usage=utils.get_cpu_usage(),
+                ram_usage=f"{utils.get_ram_usage()} MB",
+                branch=version.branch,
+            )
+            if self.config["custom_message"]
+            else (
+                f'<b>{{}}</b>\n\n<b>{{}} {self.strings("owner")}:</b> {me}\n\n<b>{{}}'
+                f' {self.strings("version")}:</b> {_version} {build}\n<b>{{}}'
+                f' {self.strings("branch")}:'
+                f"</b> <code>{version.branch}</code>\n{upd}\n\n<b>{{}}"
+                f' {self.strings("prefix")}:</b> {prefix}\n<b>{{}}'
+                f' {self.strings("uptime")}:'
+                f"</b> {utils.formatted_uptime()}\n\n<b>{{}}"
+                f' {self.strings("cpu_usage")}:'
+                f"</b> <i>~{utils.get_cpu_usage()} %</i>\n<b>{{}}"
+                f' {self.strings("ram_usage")}:'
+                f"</b> <i>~{utils.get_ram_usage()} MB</i>\n<b>{{}}</b>"
+            ).format(
+                *map(
+                    lambda x: utils.remove_html(x) if inline else x,
                     (
-                        utils.get_platform_emoji()
-                        if self._client.hikka_me.premium and not inline
-                        else "🌘 Hikka"
+                        (
+                            utils.get_platform_emoji()
+                            if self._client.hikka_me.premium and not inline
+                            else "🌘 Hikka"
+                        ),
+                        "<emoji document_id=5373141891321699086>😎</emoji>",
+                        "<emoji document_id=5469741319330996757>💫</emoji>",
+                        "<emoji document_id=5449918202718985124>🌳</emoji>",
+                        "<emoji document_id=5472111548572900003>⌨️</emoji>",
+                        "<emoji document_id=5451646226975955576>⌛️</emoji>",
+                        "<emoji document_id=5431449001532594346>⚡️</emoji>",
+                        "<emoji document_id=5359785904535774578>💼</emoji>",
+                        platform,
                     ),
-                    "<emoji document_id=5373141891321699086>😎</emoji>",
-                    "<emoji document_id=5469741319330996757>💫</emoji>",
-                    "<emoji document_id=5449918202718985124>🌳</emoji>",
-                    "<emoji document_id=5472111548572900003>⌨️</emoji>",
-                    "<emoji document_id=5451646226975955576>⌛️</emoji>",
-                    "<emoji document_id=5431449001532594346>⚡️</emoji>",
-                    "<emoji document_id=5359785904535774578>💼</emoji>",
-                    platform,
-                ),
+                )
             )
         )
-    )
 
     def _get_mark(self):
         return (
@@ -151,8 +150,7 @@ class HikkaInfoMod(loader.Module):
             "title": self.strings("send_info"),
             "description": self.strings("description"),
             **(
-                {"photo": self.config["banner_url"],
-                    "caption": self._render_info(True)}
+                {"photo": self.config["banner_url"], "caption": self._render_info(True)}
                 if self.config["banner_url"]
                 else {"message": self._render_info(True)}
             ),
@@ -193,4 +191,3 @@ class HikkaInfoMod(loader.Module):
 
         self.config["custom_message"] = args
         await utils.answer(message, self.strings("setinfo_success"))
-        
